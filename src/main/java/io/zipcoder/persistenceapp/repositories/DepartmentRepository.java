838e86ea7c43f6d709fe4a2089d41dae63142ed1
jeanvalentin51 <valentin.j.gheorghe.com>
2019-12-11 19:55:26 -0500

    Added class stubs

---
+++ src/main/java/io/zipcoder/persistenceapp/repositories/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package io.zipcoder.persistenceapp.repositories;
+
+import io.zipcoder.persistenceapp.entities.Employee;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends CrudRepository<Employee,Long> {
+
+}
